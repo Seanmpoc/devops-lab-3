4c72d180dfa07208008e63efa201569bffff4251
Sean O'Connor <merville05@gmail.com>
2024-10-10 19:17:50 +0100

    Modified hello.py to include an about page

--- hello.py
+++ hello.py
@@ -3,5 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-	return '<p>Hello, World!</p>'
+	return '''
+	<p>Hello, World!</p>
+	<p><a href="/about">Go to About Page</a></p>
+	'''
+
+@app.route('/about')
+def about():
+	return '<p>This is a Flask web app running in a Linux VM</p>'
 
